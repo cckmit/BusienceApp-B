77beb94defe0bf7cf7d388e5f26605d46d3aa13f
pjy <pjy5602@gmail.com>
2021-09-14 09:55:11 +0900

    springboot프로젝트 생성

---
+++ src/main/java/com/busience/BusienceAppAApplication.java
@@ -0,0 +1,13 @@
+package com.busience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusienceAppAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusienceAppAApplication.class, args);
+	}
+
+}
