11d1542f03f117d830848e444713865f7fd1fe30
pjy <pjy5602@gmail.com>
2021-10-06 15:57:32 +0900

    영업관리, 관제기능 컨트롤러 변경

---
+++ src/main/java/com/busience/control/controller/contolController.java
@@ -0,0 +1,45 @@
+package com.busience.control.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class contolController {
+	
+	//proMonitoring
+	@GetMapping("proMonitoring")
+	public String proMonitoring(Model model) {
+		model.addAttribute("pageName", "생산현황 모니터링");
+		return "monitoring/proMonitoring";
+	}
+	
+	//defectMonitoring
+	@GetMapping("defectMonitoring")
+	public String defectMonitoring(Model model) {
+		model.addAttribute("pageName", "불량현황 모니터링");
+		return "monitoring/defectMonitoring";
+	}
+	
+	//workMonitoring
+	@GetMapping("workMonitoring")
+	public String workMonitoring(Model model) {
+		return "workMonitoring";
+	}
+	
+	//equipMonitoring
+	@GetMapping("equipMonitoring")
+	public String equipMonitoring(Model model) {
+		return "monitoring/Graphic/Graphic1";
+	}
+	
+	@GetMapping("Graphic2")
+	public String Graphic2(Model model) {
+		return "monitoring/Graphic/Graphic2";
+	}
+	
+	@GetMapping("React1")
+	public String React1(Model model) {
+		return "React/React1";
+	}
+}
