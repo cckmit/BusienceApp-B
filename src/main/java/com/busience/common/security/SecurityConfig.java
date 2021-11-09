cd6eb69bf3a5c8cfc4fb2850d2e4943e6ca651c0
pjy <pjy5602@gmail.com>
2021-11-09 19:04:58 +0900

    no message

--- src/main/java/com/busience/common/security/SecurityConfig.java
+++ src/main/java/com/busience/common/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception{
 		
 		http.authorizeRequests()
-			.antMatchers("/", "/bsapp2").permitAll();
+			.antMatchers("/", "/bsapp2", "/productAdd").permitAll();
 		
 		http.authorizeRequests()
 			.antMatchers("/*").hasRole("ADMIN");
