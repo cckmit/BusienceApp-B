03a7ebdc0fdba5ebc71aa43c892babb87884d595
pjy <pjy5602@gmail.com>
2022-06-16 12:56:57 +0900

    사용자관리 저장 안되는 오류 수정

--- src/main/java/com/busience/standard/dao/UserDao.java
+++ src/main/java/com/busience/standard/dao/UserDao.java
@@ -3,6 +3,7 @@ package com.busience.standard.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.busience.standard.dto.UserDto;
 
@@ -19,7 +20,7 @@ public interface UserDao {
 	public int insertUser(UserDto userDto);
 	
 	//유저 등록시 메뉴관리 테이블에 메뉴리스트 추가
-	public int insertMenuNewUser(String User_Code, String User_Type);
+	public int insertMenuNewUser(@Param("User_Code") String User_Code,@Param("User_Type") String User_Type);
 	
 	//유저 수정
 	public int updateUser(UserDto userDto);
