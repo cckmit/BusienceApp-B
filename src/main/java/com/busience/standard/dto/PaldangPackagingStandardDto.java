4336e684be52ba26dfe8c667929dd639f2e82d6a
pjy <pjy5602@gmail.com>
2022-06-28 19:57:33 +0900

    포장 규격 관리 작업 완료

--- src/main/java/com/busience/standard/dto/PaldangPackagingStandardDto.java
+++ src/main/java/com/busience/standard/dto/PaldangPackagingStandardDto.java
@@ -2,9 +2,11 @@ package com.busience.standard.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class PaldangPackagingStandardDto {
 
 	private String packaging_No;
