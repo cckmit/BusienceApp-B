45bb405af59ed474a69dbc22ef18e18e30248b9e
ujin009 <65577642+ujin009@users.noreply.github.com>
2022-07-06 17:18:37 +0900

    07/06 자재 투입 현황 수정(대라벨 기준 조회 -> 소라벨 기준 조회)

--- src/main/resources/static/js/production/matInputSum.js
+++ src/main/resources/static/js/production/matInputSum.js
@@ -1,30 +1,3 @@
-var largePackTable = new Tabulator("#largePackTable",{
-	layoutColumnsOnNewData : true,
-	headerFilterPlaceholder: null,
-	height: "calc(100% - 175px)",
-	rowClick: function(e, row) {
-		smallPackTable.clearData();	
-		crateTable.clearData();
-		rawTable.clearData();				
-		row.getTable().deselectRow();
-		row.select();
-	},
-	rowSelected:function(row){
-		SIS_Search(row.getData().sales_Small_Packing_LotNo)
-    },
-	columns: [
-		{ title:"순번", field:"rownum", formatter:"rownum", hozAlign:"center"},
-		{ title: "대포장 LotNo", field: "sales_Large_Packing_LotNo", headerHozAlign: "center"},
-		{ title: "소포장 LotNo", field: "sales_Small_Packing_LotNo", headerHozAlign: "center", visible:false},
-		{ title: "제품코드", field: "sales_Packing_Code", headerHozAlign: "center"},
-		{ title: "제품명", field: "sales_Packing_Name", headerHozAlign: "center"},
-		{ title: "수량",	field: "sales_Packing_Qty",	headerHozAlign: "center", hozAlign:"right"},
-		{ title: "등록일자", field: "sales_Small_Create_Date", headerHozAlign: "center", headerHozAlign: "center", hozAlign: "center"
-		, formatter:"datetime", formatterParams:{
-    		outputFormat:"YYYY-MM-DD HH:mm" }}
-	]
-});
-
 var smallPackTable = new Tabulator("#smallPackTable", {
 	layoutColumnsOnNewData : true,
 	headerFilterPlaceholder: null,
@@ -91,21 +64,14 @@ var rawTable = new Tabulator("#rawTable", {
 	]
 });
 
-function LIS_Search() {
+function SIS_Search(){
 	
 	datas = {
 		startDate : $('#startDate').val(),
 		endDate : $('#endDate').val()
 	}
 	
-	largePackTable.setData("salesPackingRest/LargeLot_Search", datas);
-	smallPackTable.clearData();
-	crateTable.clearData();
-	rawTable.clearData();
-}
-
-function SIS_Search(largeLotNo){
-	smallPackTable.setData("smallPackagingRest/smallPackagingSelect",{LotNo: largeLotNo})
+	smallPackTable.setData("smallPackagingRest/smallPackagingSelect", datas);
 	crateTable.clearData();
 	rawTable.clearData();
 	
@@ -120,33 +86,10 @@ function RIS_Search(LotNo){
 	rawTable.setData("proListRest/rawLotList",{LotNo: LotNo})
 }
 
-$('#LIS_SearchBtn').click(function(){
-	
-	LIS_Search();	
-})
-
-$('#PRODUCT_ITEM_NAME').keypress(function(e) {
-	if (e.keyCode == 13) {
-		var value = $(this).val()
-		//내용이 있을경우 검색해서 값이 하나일경우 생략, 아닐경우 팝업창
-		$.ajax({
-			method: "GET",
-			url: "product_check?PRODUCT_ITEM_CODE=" + value,
-			dataType: "json",
-			success: function(data) {
-				if (data.length == 1) {
-					//검색어와 일치하는값이 있는경우
-					$('#PRODUCT_ITEM_CODE').val(data[0].product_ITEM_CODE);
-					$('#PRODUCT_ITEM_NAME').val(data[0].product_ITEM_NAME);
-				} else {
-					//검색어와 일치하는값이 없는경우, 팝업창
-					itemPopup(value, 'input', '', 'material');
-				}
-			}
-		})
-	}
+$('#SIS_SearchBtn').click(function(){
+	SIS_Search();	
 })
 
 $(document).ready(function() {
-	LIS_Search();
+	SIS_Search();
 });
--- src/main/webapp/WEB-INF/views/production/matInputSum.jsp
+++ src/main/webapp/WEB-INF/views/production/matInputSum.jsp
@@ -6,21 +6,21 @@
 	<div class="main">
 		<div class="top-var">
 			<div class="input-button">
-				<img id="LIS_SearchBtn" src="/images/button/Search.png"/>
+				<img id="SIS_SearchBtn" src="/images/button/Search.png"/>
 			</div>
 			<div class="input-box">
 				<div>
-					<span><strong>대포장일</strong></span>
+					<span><strong>소포장일</strong></span>
 					<input id="startDate" class="today" type="date">
 					<span style="text-align: center"><strong>~</strong></span>
 					<input id="endDate" class="tomorrow" type="date">
 				</div>
 			</div>
 		</div>
-		<div id="largePackTable" style="width: calc(25% - 10px); float:left; margin-right: 10px;"></div>
-		<div id="smallPackTable" style="width: calc(25% - 10px); float:left; margin-right: 10px;"></div>
-		<div id="crateTable" style="width: calc(25% - 10px); float:left; margin-right: 10px;"></div>
-		<div id="rawTable" style="width: calc(25% - 10px); float:left; margin-right: 10px;"></div>
+		<!-- <div id="largePackTable" style="width: calc(25% - 10px); float:left; margin-right: 10px;"></div> -->
+		<div id="smallPackTable" style="width: calc(33% - 10px); float:left; margin-right: 10px;"></div>
+		<div id="crateTable" style="width: calc(33% - 10px); float:left; margin-right: 10px;"></div>
+		<div id="rawTable" style="width: calc(34% - 1px); float:left;"></div>
 	</div>
 	<!-- END MAIN -->
 	
